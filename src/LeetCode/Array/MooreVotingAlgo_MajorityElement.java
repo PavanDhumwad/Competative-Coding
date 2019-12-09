3fbf3d3f71d7b99be90c05a787d25056ec81019c
Pavan <abc@abc.com>
2019-12-09 18:07:58 +0530

    Application of Moore Voting Algo

---
+++ src/LeetCode/Array/MooreVotingAlgo_MajorityElement.java
@@ -0,0 +1,31 @@
+package LeetCode.Array;
+
+public class MooreVotingAlgo_MajorityElement
+{
+    public static void main(String[] args)
+    {
+        int a[] = {2,2,1,1,1,2,2};
+        System.out.print("Most occurring ele(>n/2) "+mostOccurringEle(a));
+    }
+
+    private static int mostOccurringEle(int[] nums) {
+        int mostOccurring = nums[0];
+        int freq = 1;
+        for (int i=1; i<nums.length; i++)
+        {
+            if(nums[i] == mostOccurring)
+            {
+                freq++;
+            }
+            else
+                freq--;
+            if(freq==0)
+            {
+                mostOccurring = nums[i];
+                freq = 1;
+            }
+        }
+        return mostOccurring;
+    }
+
+}
