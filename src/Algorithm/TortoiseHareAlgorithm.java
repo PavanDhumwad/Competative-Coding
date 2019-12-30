f4b7a978e4c38aec471af410615ee874c4c2f61e
Pavan <abc@abc.com>
2019-12-30 13:29:01 +0530

    Tortoise-Hare Algorithm to find mid element in SLL

---
+++ src/Algorithm/TortoiseHareAlgorithm.java
@@ -0,0 +1,28 @@
+package Algorithm;
+
+import util.SLL.SLLNode;
+
+public class TortoiseHareAlgorithm
+{
+    public static void main(String[] args)
+    {
+        SLLNode head = new SLLNode(1);
+        head.next= new SLLNode(2);
+        head.next.next = new SLLNode(3);
+        head.next.next.next = new SLLNode(4);
+        head.next.next.next.next = new SLLNode(5);
+        System.out.println("Mid element is "+findMidEle(head).data);
+    }
+
+    private static SLLNode findMidEle(SLLNode head)
+    {
+        SLLNode slow = head;
+        SLLNode fast = slow.next;
+        while (fast!=null && fast.next!=null)
+        {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+}
