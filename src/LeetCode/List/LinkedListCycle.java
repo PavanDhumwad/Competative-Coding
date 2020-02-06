8a62fafb5d56dcaec2402f517a093d82a84e303f
Pavan <abc@abc.com>
2020-02-06 14:26:10 +0530

    LL Cycle with proper implementation of algo

--- src/LeetCode/List/LinkedListCycle.java
+++ src/LeetCode/List/LinkedListCycle.java
@@ -8,6 +8,9 @@ public class LinkedListCycle
         SLLNode head = new SLLNode(1);
         head.next= new SLLNode(2);
         head.next.next = new SLLNode(3);
+        head.next.next.next= new SLLNode(4);
+        head.next.next.next.next = new SLLNode(5);
+        head.next.next.next.next.next = head.next.next;
         System.out.println("Is cycle exists? "+isCycleExists(head));
     }
 
@@ -15,14 +18,14 @@ public class LinkedListCycle
     {
         if(head == null || head.next == null)
             return false;
-        SLLNode slow = head;
+        SLLNode slow = head.next;
         SLLNode fast = head.next.next;
         while(fast != null && fast.next != null)
         {
-            fast = fast.next.next;
-            slow = slow.next;
             if(fast == slow)
                 return true;
+            fast = fast.next.next;
+            slow = slow.next;
         }
         return false;
     }
